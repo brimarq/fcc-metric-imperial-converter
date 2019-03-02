4175b7fa43603b3636c70af29b1772229e75b941
Brian Marquis <rbmarquis.dev@gmail.com>
2019-03-02 01:20:27 -0600

    finish api route

--- routes/api.js
+++ routes/api.js
@@ -17,14 +17,25 @@ module.exports = function (app) {
 
   app.route('/api/convert')
     .get(function (req, res){
-      var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
-      var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      const input = req.query.input;
+      const initNum = convertHandler.getNum(input);
+      const initUnit = convertHandler.getUnit(input);
+      let returnNum, returnUnit, toString;
+
+      // Handle null values to send error responses early
+      if (!initNum || !initUnit) {
+        const errMessage = !initNum && !initUnit 
+          ? 'invalid number and unit' 
+          : !initNum ? 'invalid number' : 'invalid unit'
+        ;
+        return res.status(400).json({error: errMessage});
+      } else {
+        returnNum = convertHandler.convert(initNum, initUnit);
+        returnUnit = convertHandler.getReturnUnit(initUnit);
+        toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      }
       
-      //res.json
+      res.send({initNum, initUnit, returnNum, returnUnit, string: toString});
     });
     
 };
